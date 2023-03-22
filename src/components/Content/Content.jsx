48d6de4d8cec097da71c31a773d6a81e5dbc85dd
dokimion24 <leecy3610@gmail.com>
2023-03-22 19:58:15 +0900

    Feat: router 설정

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import AllCity from '@/components/Content/AllCity/AllCity'
+import MyCity from '@/components/Content/MyCity/MyCity'
+import Favorites from '@/components/Content/Favorites/Favorites'
 
 const Content = () => {
-  return <div>Content</div>
+  return (
+    <Routes>
+      <Route path="/" element={<MyCity />} />
+      <Route path="/cities" element={<AllCity />} />
+      <Route path="/favorites" element={<Favorites />} />
+    </Routes>
+  )
 }
 
 export default Content
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { Link } from 'react-router-dom'
 
 const Menu = () => {
-  return <div>Menu</div>
+  return (
+    <div>
+      <button>
+        <Link to="/">내 지역보기</Link>
+      </button>
+      <button>
+        <Link to="/cities">전체 시도보기</Link>
+      </button>
+      <button>
+        <Link to="/favorites">즐겨찾기</Link>
+      </button>
+    </div>
+  )
 }
 
 export default Menu
