400c9eb44b7d4bd48f0575aa178bc93c5c9560a5
dokimion24 <leecy3610@gmail.com>
2023-03-23 12:01:37 +0900

    Conf: proxy 설정

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://apis.data.go.kr',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
