b45e595abdb36b465c83b975094011673eb751b5
dokimion24 <leecy3610@gmail.com>
2023-03-24 14:38:47 +0900

    Feat: redux provider 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import store from '@/store'
+
 import Header from '@/components/Header/Header'
 import Content from '@/components/Content/Content'
 import Menu from '@/components/Menu/Menu'
 import GlobalStyle from './styles/Global'
-
 import styled from '@emotion/styled'
 
 const Container = styled.div`
@@ -14,14 +16,14 @@ const Container = styled.div`
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <Container>
         <Header />
         <Content />
         <Menu />
       </Container>
-    </>
+    </Provider>
   )
 }
 
