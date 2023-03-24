840b23778fa9f68ba1762ccbd7497c7788306bc9
dokimion24 <leecy3610@gmail.com>
2023-03-24 21:08:22 +0900

    Feat: PollutionList 컴포넌트 추가

--- src/components/Content/MyCity/MyCIty.jsx
+++ src/components/Content/MyCity/MyCIty.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import { fetchData } from '@/modules/dust'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import PollutionInfoList from '../../Pollution/PollutionInfoList/PoulltionInfoList'
 
 const MyCity = () => {
   // const ee = useSelector((state) => state.data)
   const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(fetchData('대구'))
-  }, [dispatch])
+  // useEffect(() => {
+  //   dispatch(fetchData('대구'))
+  // }, [dispatch])
 
-  return <div>MyCity</div>
+  return (
+    <div>
+      <PollutionInfoList />
+    </div>
+  )
 }
 
 export default MyCity
