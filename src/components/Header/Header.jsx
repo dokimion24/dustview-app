cfa67169a064015d766eaffd3987c63dfd806d10
dokimion24 <leecy3610@gmail.com>
2023-03-24 21:07:54 +0900

    Feat: 도시 설정 후  데이터 가져오기

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { fetchData } from '@/modules/dust'
+
 import { useState } from 'react'
-import { dispatch } from 'gatsby-cli/lib/reporter/redux'
 
 const Header = () => {
   const [city, setCity] = useState()
+  const dispatch = useDispatch()
 
   const HandleSelectCity = (e) => {
-    console.log(e.target.value)
-    // setCity()
-    // dispatch(fetchData(city))
+    setCity(e.target.value)
   }
+
+  useEffect(() => {
+    dispatch(fetchData(city))
+  }, [city])
 
   return (
     <div>
       <label>Filter by year</label>
       <select name="cities" onChange={HandleSelectCity}>
-        <option value="서울" selected>
-          서울
-        </option>
+        <option value="서울">서울</option>
         <option value="부산">부산</option>
         <option value="대구">대구</option>
         <option value="인천">인천</option>
