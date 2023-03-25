16b2619398c087558f89e565199db42ed974a1c7
dokimion24 <leecy3610@gmail.com>
2023-03-25 17:36:42 +0900

    Remove: 파일삭제

--- src/components/Content/AllCity/AllCity.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const AllCity = () => {
-  return <div>AllCity</div>
-}
-
-export default AllCity
--- src/components/Content/Favorites/Favorites.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Favorites = () => {
-  return <div>Favorites Favorites</div>
-}
-
-export default Favorites
