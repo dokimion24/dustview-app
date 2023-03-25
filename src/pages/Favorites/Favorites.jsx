41459c70b7f1228046eab957d49232aac94b35cb
dokimion24 <leecy3610@gmail.com>
2023-03-25 17:39:42 +0900

    Feat: Favorites 컴포넌트 작성

---
+++ src/pages/Favorites/Favorites.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Menu from '@/components/Menu/Menu'
+
+
+
+const Favorites = () => {
+  return (
+    <div>
+      <Menu />
+    </div>
+  )
+}
+
+export default Favorites
