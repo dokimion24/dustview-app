be3b77828f86ef0b547bb945fd55d6705dd003d0
dokimion24 <leecy3610@gmail.com>
2023-03-25 17:37:42 +0900

    Feat: 아이콘 추가, 즐겨찾기 dispatch 추가

--- src/components/Pollution/PollutionInfoItem/PollutionInfoItem.jsx
+++ src/components/Pollution/PollutionInfoItem/PollutionInfoItem.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import { formatGrade } from '@/utils/formatGrade'
 
-const PollutionItem = ({ PollutionInfo }) => {
-  console.log(PollutionInfo)
+import { AiOutlineStar } from 'react-icons/ai'
+import * as S from '@/components/Pollution/PollutionInfoItem/PollutionInfoItem.style'
+import { toggleFavReducer, addFav, deleteFav } from '@/modules/favorities'
+
+import { useDispatch } from 'react-redux'
+
+const PollutionItem = ({ pollutionInfo }) => {
+  const dispatch = useDispatch()
+
+  const HandleToggleFav = () => {
+    dispatch(addFav(pollutionInfo))
+  }
+
   return (
     <>
       <div>
-        <h2>{PollutionInfo.stationName}</h2>
-        <span>{PollutionInfo.sidoName}</span>
+        <h2>{pollutionInfo.stationName}</h2>
+        <span>{pollutionInfo.sidoName}</span>
       </div>
-      <div>{formatGrade(PollutionInfo.pm10Grade)}</div>
+      <div>{formatGrade(pollutionInfo.pm10Grade)}</div>
       <div>
-        <span>미세먼지 수치 : {PollutionInfo.pm10Value}</span>
-        <div>{PollutionInfo.dataTime}</div>
+        <span>미세먼지 수치 : {pollutionInfo.pm10Value}</span>
+        <div>{pollutionInfo.dataTime}</div>
       </div>
+
+      <S.Icon onClick={HandleToggleFav}>
+        <AiOutlineStar color="red" />
+      </S.Icon>
     </>
   )
 }
