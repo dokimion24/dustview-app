58bbaa6471e49a92f50ddf34c64b26c87c7bb302
dokimion24 <leecy3610@gmail.com>
2023-03-25 17:41:45 +0900

    Feat: 즐겨찾기 modules작성

---
+++ src/modules/favorities.js
@@ -0,0 +1,23 @@
+const ADD_FAV = 'favorities/ADD_FAV'
+const DELETE_FAV = 'favorities/DELETE_FAV'
+
+export function addFav(favorities) {
+  return { type: ADD_FAV, payload: favorities }
+}
+
+export function deleteFav(favorities) {
+  return { type: DELETE_FAV, payload: favorities }
+}
+
+const initialState = []
+
+export const toggleFavReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_FAV:
+      return [...state, action.payload]
+    case DELETE_FAV:
+      return state.filter((element) => element.id !== action.payload.id)
+    default:
+      return state
+  }
+}
