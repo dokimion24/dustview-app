2b015440f6864f9ef7eda0b19335937222c07414
dokimion24 <leecy3610@gmail.com>
2023-03-25 17:39:22 +0900

    Feat: toggleReduecer 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux'
 import { combineReducers } from 'redux'
-import { fetchReducer } from '../modules/dust'
+import { fetchReducer } from '@/modules/dust'
+import { toggleFavReducer } from '@/modules/favorities'
 import ReduxThunk from 'redux-thunk'
 import logger from 'redux-logger'
 
 const rootReducer = combineReducers({
   fetchReducer,
+  toggleFavReducer,
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
