bd1cbf87bbe4cfd63850dfedfd988f234ac8e88b
dokimion24 <leecy3610@gmail.com>
2023-03-26 17:52:46 +0900

    Feat: favorites 페이지 즐겨찾기 추가

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Menu from '@/components/Menu/Menu'
-
-
+import { useSelector } from 'react-redux'
+import PollutionInfoList from '@/components/Pollution/PollutionInfoList/PollutionInfoList'
 
 const Favorites = () => {
+  const pollutionInfos = useSelector((state) => state.toggleFavReducer)
+  console.log(pollutionInfos)
+  console.log('...render')
+
   return (
     <div>
       <Menu />
+      <PollutionInfoList pollutionInfos={pollutionInfos} />
     </div>
   )
 }
