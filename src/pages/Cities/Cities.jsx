67e954ac94093034483b914b903a54a79f0eecd6
dokimion24 <leecy3610@gmail.com>
2023-03-26 18:01:27 +0900

    Feat: 로딩 처리 주석

--- src/pages/Cities/Cities.jsx
+++ src/pages/Cities/Cities.jsx
@@ -5,20 +5,22 @@ import CitySelect from '@/components/CitySelect/CitySelect'
 import { useSelector } from 'react-redux'
 
 const Cities = () => {
-  const { data, loading, error } = useSelector((state) => state.fetchReducer)
+  const data = useSelector((state) => state.fetchReducer.data)
 
-  if (loading) return <div>로딩중...</div>
-  if (error) return <div>에러 발생</div>
-  if (!data) return null
+  console.log('...render')
 
+  // if (loading) return <div>로딩중...</div>
+  // if (error) return <div>에러 발생</div>
+  // if (!data) return null
+
+  const pollutionInfos = data?.response?.body?.items
   // console.log('Cities data pollutionInfos,', pollutionInfos)
-  const pollutionInfos = data.response.body.items
-  console.log('Cities', data.response.body.items)
+  // console.log('Cities', data.response.body.items)
 
   return (
     <div>
       <CitySelect />
-      <PollutionInfoList pollutionInfos={pollutionInfos} />
+      {pollutionInfos && <PollutionInfoList pollutionInfos={pollutionInfos} />}
       <Menu />
     </div>
   )
