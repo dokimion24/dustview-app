fe60e5ee589037eaeaaa6e11b7f814af8011d959
dokimion24 <leecy3610@gmail.com>
2023-03-26 17:51:26 +0900

    Feat: 주석 삭제

--- src/components/Pollution/PollutionInfoList/PollutionInfoList.jsx
+++ src/components/Pollution/PollutionInfoList/PollutionInfoList.jsx
@@ -3,17 +3,6 @@ import { useSelector } from 'react-redux'
 import PollutionInfoItem from '../PollutionInfoItem/PollutionInfoItem'
 
 const PollutionInfoList = ({ pollutionInfos }) => {
-  // const { data, loading, error } = useSelector((state) => state.fetchReducer)
-
-  // if (loading) return <div>로딩중...</div>
-  // if (error) return <div>에러 발생</div>
-  // if (!data) return null
-
-  // const pollutionInfos = data.response.body.items
-  // console.log(pollutionInfos)
-
-  console.log(pollutionInfos)
-
   return (
     <div>
       {pollutionInfos.map((pollutionInfo) => (
