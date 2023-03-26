389241fe5d4d02023e4a84f00523c8ab1bd04c7b
dokimion24 <leecy3610@gmail.com>
2023-03-26 17:51:48 +0900

    Feat: selectCityReducer 추가

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { applyMiddleware, compose, legacy_createStore as createStore } from 'red
 import { combineReducers } from 'redux'
 import { fetchReducer } from '@/modules/dust'
 import { toggleFavReducer } from '@/modules/favorities'
+import { selectCityReducer } from '@/modules/city'
 import ReduxThunk from 'redux-thunk'
 import logger from 'redux-logger'
 
 const rootReducer = combineReducers({
   fetchReducer,
   toggleFavReducer,
+  selectCityReducer,
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
