8c89343a90e0e41f1356ca8b39ab7ec52a2869ec
dokimion24 <leecy3610@gmail.com>
2023-03-27 22:02:54 +0900

    Feat: select 스타일 지정

--- src/pages/MyCity/MyCity.jsx
+++ src/pages/MyCity/MyCity.jsx
@@ -3,15 +3,18 @@ import Menu from '@/components/Menu/Menu'
 import CitySelect from '@/components/CitySelect/CitySelect'
 import StationSelect from '@/components/StationSelect/StationSelect'
 import PollutionItem from '@/components/Pollution/PollutionInfoItem/PollutionInfoItem'
-import { useSelector } from 'react-redux'
+
+import * as S from '@/styles/Common/Common.style'
 
 const MyCity = () => {
-  const [station, setStation] = useState()
+  const [station, setStation] = useState('중구')
 
   return (
     <div>
-      <CitySelect />
-      <StationSelect setStation={setStation} />
+      <S.Header>
+        <CitySelect />
+        <StationSelect setStation={setStation} />
+      </S.Header>
       {station && <PollutionItem pollutionInfo={station} />}
       <Menu />
     </div>
