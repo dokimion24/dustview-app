ccfe0971c856c4a2b23180d1d797b0f18693b03a
dokimion24 <leecy3610@gmail.com>
2023-03-28 22:11:29 +0900

    Feat: station dispatch

--- src/components/StationSelect/StationSelect.jsx
+++ src/components/StationSelect/StationSelect.jsx
@@ -1,27 +1,36 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { selectCity } from '@/modules/city'
 
-const StationSelect = ({ setStation }) => {
-  const { data, loading, error } = useSelector((state) => state.fetchReducer)
+import * as S from '@/components/StationSelect/StationSelect.style'
+
+const StationSelect = ({ setPollutionInfo }) => {
+  console.log('..............')
+  const data = useSelector((state) => state.fetchReducer.data)
+  const city = useSelector((state) => state.selectCityReducer)
+
+  const selectCitydispatch = useDispatch()
+
   const pollutionInfos = data?.response?.body?.items
 
   const HandleSelectStation = (e) => {
-    const filteredPollutionInfo = pollutionInfos.filter(
+    const filteredPollutionInfo = pollutionInfos.find(
       (pollutionInfo) => pollutionInfo.stationName === e.target.value,
     )
-    setStation(...filteredPollutionInfo)
+    setPollutionInfo(filteredPollutionInfo)
+    selectCitydispatch(selectCity(city.city, filteredPollutionInfo.stationName))
   }
 
   return (
-    <div>
-      <select name="station" onChange={HandleSelectStation}>
+    <>
+      <S.Select name="station" onChange={HandleSelectStation} value={city.station}>
         {pollutionInfos?.map((pollutionInfo) => (
           <option value={pollutionInfo.stationName} key={pollutionInfo.stationName}>
             {pollutionInfo.stationName}
           </option>
         ))}
-      </select>
-    </div>
+      </S.Select>
+    </>
   )
 }
 
