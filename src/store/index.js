9ee47d9148ef373f395bb57ce946b19f27fd6db2
dokimion24 <leecy3610@gmail.com>
2023-03-30 13:24:02 +0900

    Feat: 기존 store 주석 처리, slice 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,41 @@
-import { applyMiddleware, legacy_createStore as createStore } from 'redux'
-import { combineReducers } from 'redux'
-import { fetchReducer } from '@/modules/dust'
-import { toggleFavReducer } from '@/modules/favorities'
-import { selectCityReducer } from '@/modules/city'
-import ReduxThunk from 'redux-thunk'
-import logger from 'redux-logger'
+// import { applyMiddleware, legacy_createStore as createStore } from 'redux'
+// import { combineReducers } from 'redux'
+// import { fetchReducer } from '@/modules/dust'
+// import { toggleFavReducer } from '@/modules/favorities'
+// import { selectCityReducer } from '@/modules/city'
+// import ReduxThunk from 'redux-thunk'
+// import logger from 'redux-logger'
 
-import storage from 'redux-persist/lib/storage'
-import { persistReducer, persistStore } from 'redux-persist'
+// import storage from 'redux-persist/lib/storage'
+// import { persistReducer, persistStore } from 'redux-persist'
+
+// const rootReducer = combineReducers({
+//   fetchReducer,
+//   toggleFavReducer,
+//   selectCityReducer,
+// })
+
+// const persistConfig = {
+//   key: 'root',
+//   storage: storage,
+//   whitelist: ['toggleFavReducer'],
+// }
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// export const store = createStore(persistedReducer, applyMiddleware(ReduxThunk, logger))
+// export const persistor = persistStore(store)
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { createLogger } from 'logger'
+import { citySlice } from './citySlice'
 
 const rootReducer = combineReducers({
-  fetchReducer,
-  toggleFavReducer,
-  selectCityReducer,
+  [citySlice.name]: citySlice.reducer,
 })
 
-const persistConfig = {
-  key: 'root',
-  storage: storage,
-  whitelist: ['toggleFavReducer'],
-}
+const store = configureStore({
+  reducer: rootReducer,
+})
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-export const store = createStore(persistedReducer, applyMiddleware(ReduxThunk, logger))
-export const persistor = persistStore(store)
+export default store
